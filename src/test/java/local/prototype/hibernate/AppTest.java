1869596bc99f22fffed185bc354deb1f871a2741
Paul Chiu <paulchiu@gmail.com>
2013-05-23 12:50:11 +1000

    Refactored code and updated project name.

--- src/test/java/local/prototype/hibernate/AppTest.java
+++ src/test/java/local/prototype/hibernate/AppTest.java
@@ -24,31 +24,42 @@ package local.prototype.hibernate;
  * Boston, MA  02110-1301  USA
  */
 
-import java.util.Date;
-import java.util.List;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
-import org.hibernate.service.*;
-import org.hibernate.*;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Test based on work by Steve Ebersole downloaded from http://javasourcecode.org/html/open-source/hibernate/hibernate-3.6.0.Final/org/hibernate/tutorial/annotations/AnnotationsIllustrationTest.java.html
- *
+ * <p/>
  * Successful tests confirms the correct installation/configuration of Hibernate with annotations.
  *
  * @author Paul Chiu
-  */
-public class AppTest 
-    extends TestCase
-{
-
+ */
+public class AppTest extends TestCase {
+    public static final String EVENT_ONE_TITLE = "Our very first event!";
+    public static final String EVENT_TWO_TITLE = "A follow up event";
+    public static final String UPDATED_EVENT_TITLE = "Updated event!";
     private SessionFactory sessionFactory;
     private ServiceRegistry serviceRegistry;
     private Session session;
+
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
 
     @Override
     protected void setUp() throws Exception {
@@ -60,50 +71,40 @@ public class AppTest
 
     @Override
     protected void tearDown() throws Exception {
-        if ( sessionFactory != null ) {
+        if (sessionFactory != null) {
             sessionFactory.close();
         }
     }
 
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 
     public void testInsert() {
         session = sessionFactory.openSession();
         session.beginTransaction();
-        session.save( new Event( "Our very first event!", new Date() ) );
-        session.save( new Event( "A follow up event", new Date() ) );
+        session.save(new Event(EVENT_ONE_TITLE, new Date()));
+        session.save(new Event(EVENT_TWO_TITLE, new Date()));
         session.getTransaction().commit();
         session.close();
-    }        
-
-    public void testSelect() {
-        List result = getEvents();
-        for ( Event event : (List<Event>) result )
-            System.out.println( "Event (" + event.getId() + ") : " + event.getTitle() );
     }
 
-    private List getEvents() {
+    public void testSelect() {
+        List<Event> result = getEvents();
+        for (Event event : result)
+            System.out.println("Event (" + event.getId() + ") : " + event.getTitle());
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Event> getEvents() {
         session = sessionFactory.openSession();
-        List result = session.createQuery( "from Event" ).list();
+        List<Event> result = session.createQuery("from Event").list();
         session.close();
         return result;
     }
 
     public void testSelectById() {
-        assertEquals("Title match", getEventById(1L).getTitle(), "Our very first event!");
+        assertEquals("Title match", getEventById(1L).getTitle(), EVENT_ONE_TITLE);
     }
 
     private Event getEventById(Long id) {
@@ -111,21 +112,9 @@ public class AppTest
         Query selectQuery = getSelectByIdQuery(id);
         List results = selectQuery.list();
         if (results.size() == 1)
-            return (Event)results.get(0);
+            return (Event) results.get(0);
         else
             return new Event();
-    }
-
-    public void testUpdate() {
-        Event eventOld = getEventById(1L);
-        eventOld.setTitle("hello world");
-        session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.update(eventOld);
-        session.getTransaction().commit();
-        session.close();
-        Event eventNew = getEventById(1L);
-        assertEquals("Updated title match", eventOld.getTitle(), eventNew.getTitle());
     }
 
     private Query getSelectByIdQuery(Long id) {
@@ -134,15 +123,25 @@ public class AppTest
         return query;
     }
 
-    public void testDelete() {
-        List result = getEvents();
+    public void testUpdate() {
+        Event eventOld = getEventById(1L);
+        eventOld.setTitle(UPDATED_EVENT_TITLE);
         session = sessionFactory.openSession();
         session.beginTransaction();
-        for ( Event event : (List<Event>) result )
+        session.update(eventOld);
+        session.getTransaction().commit();
+        session.close();
+        assertEquals("Updated title match", eventOld.getTitle(), getEventById(1L).getTitle());
+    }
+
+    public void testDelete() {
+        List<Event> result = getEvents();
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        for (Event event : result)
             session.delete(event);
         session.getTransaction().commit();
         session.close();
-
         assertEquals("All events deleted", 0, getEvents().size());
     }
 }
